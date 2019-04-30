5cbfb5a6dba4b43305ae21e6a7072e95a1b54e78
Shefali Agarwal <agars979@newschool.edu>
2019-04-29 20:19:36 -0400

    Draw on grid!!!

--- exercise-1/assets/js/app.js
+++ exercise-1/assets/js/app.js
@@ -1,4 +1,22 @@
 var color = "black";
+var drag = false;
+
+$(".outline").mousedown(function() {
+    drag = true;
+});
+
+$(".outline").mouseup(function() {
+    drag = false;
+});
+
+$(".bar").mouseover(function() {
+    if (drag) {
+        /*if ($(this).hasClass("selected"))
+            $(this).removeClass(`selected ${color}`);
+        else*/
+            $(this).addClass(`selected ${color}`);
+    }
+});
 
 $(".bar").click(function() {
     if ($(this).hasClass("selected"))
